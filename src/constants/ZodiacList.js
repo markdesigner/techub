87d444da1c0ce88b029341bc140363a0cbf15469
mark <mark.chang@vivotek.com>
2024-01-12 15:06:07 +0800

    feat: revise zadiac view

--- src/constants/ZodiacList.js
+++ src/constants/ZodiacList.js
@@ -1,20 +1,172 @@
 const zodiacList = [
   {
+    name: 'Aries',
+    chineseName: '牡羊座',
+    duration: {
+      start: {
+        month: '03',
+        date: '21'
+      },
+      end: {
+        month: '04',
+        date: '19'
+      }
+    }
+  },
+  {
+    name: 'Taurus',
+    chineseName: '金牛座',
+    duration: {
+      start: {
+        month: '04',
+        date: '20'
+      },
+      end: {
+        month: '05',
+        date: '20'
+      }
+    }
+  },
+  {
+    name: 'Gemini',
+    chineseName: '雙子座',
+    duration: {
+      start: {
+        month: '05',
+        date: '21'
+      },
+      end: {
+        month: '06',
+        date: '20'
+      }
+    }
+  },
+  {
+    name: 'Cancer',
+    chineseName: '巨蟹座',
+    duration: {
+      start: {
+        month: '06',
+        date: '21'
+      },
+      end: {
+        month: '07',
+        date: '22'
+      }
+    }
+  },
+  {
+    name: 'Leo',
+    chineseName: '獅子座',
+    duration: {
+      start: {
+        month: '07',
+        date: '23'
+      },
+      end: {
+        month: '08',
+        date: '22'
+      }
+    }
+  },
+  {
+    name: 'Virgo',
+    chineseName: '處女座',
+    duration: {
+      start: {
+        month: '08',
+        date: '23'
+      },
+      end: {
+        month: '09',
+        date: '22'
+      }
+    }
+  },
+  {
+    name: 'Libra',
+    chineseName: '天秤座',
+    duration: {
+      start: {
+        month: '09',
+        date: '23'
+      },
+      end: {
+        month: '10',
+        date: '22'
+      }
+    }
+  },
+  {
+    name: 'Scorpio',
+    chineseName: '天蠍座',
+    duration: {
+      start: {
+        month: '10',
+        date: '23'
+      },
+      end: {
+        month: '11',
+        date: '21'
+      }
+    }
+  },
+  {
     name: 'Sagittarius',
     chineseName: '射手座',
     duration: {
-      start: '2023-11-23T08:00:00',
-      end: '2023-12-21T08:00:00'
+      start: {
+        month: '11',
+        date: '22'
+      },
+      end: {
+        month: '12',
+        date: '21'
+      }
+    }
+  },
+  {
+    name: 'Capricorn',
+    chineseName: '摩羯座',
+    duration: {
+      start: {
+        month: '12',
+        date: '22'
+      },
+      end: {
+        month: '01',
+        date: '19'
+      }
     }
   },
   {
     name: 'Aquarius',
     chineseName: '水瓶座',
     duration: {
-      start: '2023-01-21T08:00:00',
-      end: '2023-02-19T08:00:00'
+      start: {
+        month: '01',
+        date: '20'
+      },
+      end: {
+        month: '02',
+        date: '18'
+      }
+    }
+  },
+  {
+    name: 'Pisces',
+    chineseName: '雙魚座',
+    duration: {
+      start: {
+        month: '02',
+        date: '19'
+      },
+      end: {
+        month: '03',
+        date: '20'
+      }
     }
   }
-]
+];
 
-export { zodiacList }
+export default zodiacList;
--- src/views/ZadiacSignView.vue
+++ src/views/ZadiacSignView.vue
@@ -1,27 +1,45 @@
 <script setup>
 import { ref } from 'vue'
-import { zodiacList } from '@/constants/ZodiacList'
+import zodiacList from '@/constants/ZodiacList'
 
 let date = ref(new Date())
 let signName = ref('')
 const checkSign = () => {
-  console.log('checkSign', checkSign)
-
-  const dates = new Date(date.value)
-  const timestamp = dates.getTime()
+  const { timestamp, month, day } = convertSelectorTimeToSameYearTimestamp(date)
   for (let sign of zodiacList) {
-    const startTimestamp = new Date(sign.duration.start).getTime()
-    const endTimestamp = new Date(sign.duration.end).getTime()
-    console.log(timestamp, startTimestamp, endTimestamp, 'aaa')
-
+    const { startTimestamp, endTimestamp } = convertToSameYearTimestamp(sign)
+    if(month === '01' && Number(day) <= 19) {
+      const sign =  zodiacList.find(sign => sign.name === 'Capricorn');
+      signName.value = sign.chineseName
+      return 
+    }
     if (timestamp >= startTimestamp && timestamp <= endTimestamp) {
       signName.value = sign.chineseName
       return
-    } else {
-      signName.value = '博愛座'
-      return
     }
   }
+}
+
+const convertToSameYearTimestamp = (sign) => {
+  const startTimeString = `2023-${sign.duration.start.month}-${sign.duration.start.date}T00:00:00`
+  const endTimeString =
+    sign.name === 'Capricorn'
+      ? `2024-${sign.duration.end.month}-${sign.duration.end.date}T00:00:00`
+      : `2023-${sign.duration.end.month}-${sign.duration.end.date}T00:00:00`
+  const startTimestamp = new Date(startTimeString).getTime()
+  const endTimestamp = new Date(endTimeString).getTime()
+  return { startTimestamp, endTimestamp }
+}
+
+const convertSelectorTimeToSameYearTimestamp = (selectTime) => {
+  const date = new Date(selectTime.value)
+  const month = `00${date.getUTCMonth() + 1}`.substr(-2)
+  const day = `00${date.getUTCDate()}`.substr(-2)
+  const timeString = `2023-${month}-${day}T00:00:00`
+  console.log(timeString,'timeString')
+  const timestamp = new Date(timeString).getTime()
+  console.log(timestamp,'user impiut')
+  return { timestamp, month, day }
 }
 
 // watchEffect(() => { console.log(123123);
